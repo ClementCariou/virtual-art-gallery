6f05f04ecddfd7ca6f22a1f29b2580dcbffb1fba
Clément Cariou <email.clement.cariou@gmail.com>
2020-05-20 13:32:19 -0400

    Error message

--- src/index.js
+++ src/index.js
@@ -13,47 +13,51 @@ const regl = require('regl')({
 	]
 });
 
-window.addEventListener('resize', fit(canvas), false);
-const map = require('./map')();
-const drawMap = require('./mesh')(regl, map);
-const placement = require('./placement')(regl, map.segments);
-const drawPainting = require('./painting')(regl);
-const fps = require('./fps')(map.segments);
+try {
+	window.addEventListener('resize', fit(canvas), false);
+	const map = require('./map')();
+	const drawMap = require('./mesh')(regl, map);
+	const placement = require('./placement')(regl, map.segments);
+	const drawPainting = require('./painting')(regl);
+	const fps = require('./fps')(map.segments);
 
-const proj = [];
-const context = regl({
-	cull: {
-		enable: true,
-		face: 'back'
-	},
-	uniforms: {
-		view: fps.view,
-		yScale: 1.0,
-		proj: ({ viewportWidth, viewportHeight }) =>
-			mat4.perspective(proj, Math.PI / 3, viewportWidth / viewportHeight, 0.1, 100)
-	}
-});
+	const proj = [];
+	const context = regl({
+		cull: {
+			enable: true,
+			face: 'back'
+		},
+		uniforms: {
+			view: fps.view,
+			yScale: 1.0,
+			proj: ({ viewportWidth, viewportHeight }) =>
+				mat4.perspective(proj, Math.PI / 3, viewportWidth / viewportHeight, 0.1, 100)
+		}
+	});
 
-const reflexion = regl({
-	cull: {
-		enable: true,
-		face: 'front'
-	},
-	uniforms: {
-		yScale: -1.0
-	}
-});
+	const reflexion = regl({
+		cull: {
+			enable: true,
+			face: 'front'
+		},
+		uniforms: {
+			yScale: -1.0
+		}
+	});
 
-regl.frame(({ time }) => {
-	fps.tick({ time });
-	placement.update(fps.pos);
-	regl.clear({ color: [0, 0, 0, 1] });
-	context(() => {
-		reflexion(() => {
+	regl.frame(({ time }) => {
+		fps.tick({ time });
+		placement.update(fps.pos);
+		regl.clear({ color: [0, 0, 0, 1] });
+		context(() => {
+			reflexion(() => {
+				drawMap();
+				drawPainting(placement.batch());
+			});
 			drawMap();
 			drawPainting(placement.batch());
 		});
-		drawMap();
-		drawPainting(placement.batch());
 	});
-});
+} catch (e) {
+	document.body.innerText = "This project doesn't work on certain browser";
+}
