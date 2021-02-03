4510bc274f7c1538d2ecc171a90b57ca51a141ce
Clément Cariou <email.clement.cariou@gmail.com>
2021-02-04 00:27:33 +0100

    Don't repeat images

--- api/local.js
+++ api/local.js
@@ -4,7 +4,7 @@
 const images = require("../images/images.json").images.map((img,i)=>({...img, image_id:i}));
 module.exports = {
     fetchList: async function (from, count) {
-        return images.map((img,i)=>({...img, image_id:i+from}));//.slice(from, from + count);
+        return images.slice(from, from + count);//.map((img,i)=>({...img, image_id:i+from}));
     },
     fetchImage: async function (obj, advicedResolution) {
         const url = "images/" + obj.file;
