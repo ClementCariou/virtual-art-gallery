9bc6b986d731a36461a301b64f9ff7f749ddee20
Illya Moskvin <imoskvin@artic.edu>
2021-08-09 17:44:29 -0500

    Only show public domain images

--- api/artic.js
+++ api/artic.js
@@ -3,7 +3,7 @@
 // ARTIC API
 const searchURL = 'https://api.artic.edu/api/v1/artworks/search';
 const imageURL = ({image_id}, res) => `https://www.artic.edu/iiif/2/${image_id}/full/,${res}/0/default.jpg`;
-const query = '?query[bool][must][][term][classification_titles.keyword]=painting';
+const query = '?query[bool][must][][term][classification_titles.keyword]=painting&query[bool][must][][term][is_public_domain]=true';
 const fields = '&fields=image_id,title,artist_title';
 module.exports = {
     fetchList: async function (from, count) {
